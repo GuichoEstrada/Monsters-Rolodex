5ce629b5f69e4a1395a043a9cfcf5420a28fa9ee
Carlo Estrada <lcrestrada.dev@gmail.com>
2020-06-03 12:39:42 +0800

    adding files for github pages

---
+++ src/components/search-bar/search-bar.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './search-bar.styles.css';
+
+
+export const SearchBar = ({placeholder, handleChange}) => {
+    return (
+        <input
+            className='search-bar'
+            type='search'
+            placeholder={placeholder}
+            onChange={handleChange}
+        />
+    )
+}
---
+++ src/components/title/title.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './title.styles.css';
+
+export const Title = () => {
+    return (
+        <h1 className='title'>
+            Monsters Rolodex
+        </h1>
+    );
+}
